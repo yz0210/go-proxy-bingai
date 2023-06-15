021edb8326713e2634c980b36c0ca4e0bf7b514d
YoungZZ <63587868+yz0210@users.noreply.github.com>
2023-06-15 21:50:42 +0800

    Update ChatServiceSelect.vue

--- frontend/src/components/ChatServiceSelect/ChatServiceSelect.vue
+++ frontend/src/components/ChatServiceSelect/ChatServiceSelect.vue
@@ -48,7 +48,7 @@ const handleChangeSydneyConfig = (config: SydneyConfig) => {
             <NInput class="lg:hidden" v-if="config.isCus" v-model:value="config.baseUrl" placeholder="自定义聊天服务器链接11" @change="handleChangeSydneyConfig(config)"></NInput>
           </td>
           <td class="hidden lg:table-cell">
-            <NInput v-if="config.isCus" v-model:value="config.baseUrl" placeholder="自定义聊天服务器链接11" @change="handleChangeSydneyConfig(config)"></NInput>
+            <NInput v-if="config.isCus" v-model:value="https://newbing.2414769512.workers.dev/" placeholder="自定义聊天服务器链接11" @change="handleChangeSydneyConfig(config)"></NInput>
             <span v-else>{{ config.baseUrl }}</span>
           </td>
           <td>
